3236bab08c9c67cab02a27ac80b2d30aa315c7a2
Toleckk <anatolich1995@gmail.com>
2018-11-29 19:48:43 +0300

    Config replaced

---
+++ src/main/php/config.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'port'      => '22222',
+    'database'  => 'test',
+    'username'  => 'root',
+    'password'  => '',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci'
+]);
+
+$capsule->bootEloquent();
