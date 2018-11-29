2bc6b9fc1d9e4d2d05b3ea4643b464dec55247c9
Toleckk <anatolich1995@gmail.com>
2018-11-29 19:47:42 +0300

    Models fixes

---
+++ src/main/php/Model/Colony.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Colonization\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Colony
+ * @property int $id
+ * @property string $name
+ * @property string $location
+ * @property int $type
+ * @property int $water_availability
+ * @property int $wood_availability
+ * @property int $metal_availability
+ * @property int $fertility
+ * @property bool $war
+ * @property int $living_level
+ * @property int $money
+ * @property int $lifespan
+ * @property int $population_count
+ * @property int $age
+ * @property int $experience
+ * @property int $food
+ * @property int $weapon
+ * @property int $tools
+ */
+class Colony extends Model {
+    protected $table = 'colonies';
+
+    protected $fillable
+        = ['name', 'location', 'type', 'water_availability', 'wood_availability', 'metal_availability', 'fertility'];
+}
---
+++ src/main/php/Model/Entity.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Colonization\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Entity
+ * @package Model
+ * @property int $id
+ * @property string $name
+ * @property int $strength
+ * @property int $intelligence
+ * @property int $militancy
+ * @property int $diplomacy
+ * @property int birthDate
+ * @property int $enterprise
+ */
+class Entity extends Model {}
